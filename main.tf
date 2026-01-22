1a2ba2f079318de055af9d3a130f92565444534a
NeelimaNair <neelimarun@gmail.com>
2026-01-22 16:39:03 +1100

    Updating dns attributes at VPC level

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ provider "aws" {
 # -------------------------------
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
 
 resource "aws_subnet" "private_a" {
