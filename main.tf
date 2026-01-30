cbcabcfaa4b651f5ef7330007f6d224e8a37bf43
NeelimaNair <neelimarun@gmail.com>
2026-01-30 17:12:43 +1100

    Corrected security group to pick VPC cidr block

--- main.tf
+++ main.tf
@@ -179,7 +179,7 @@ resource "aws_security_group" "sgr_mel_datasphere_alb" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] # Restrict to VPC
+    cidr_blocks = [aws_vpc.main.cidr_block] # Restrict to VPC
   }
 
   egress {
